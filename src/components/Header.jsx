08034cbe281234be63353227c63f064189a56c3b
robinbraemer <robin.braemer@web.de>
2024-03-22 05:37:09 +0100

    update mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,11 +79,14 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
           >
-            <MobileNavLink href="#features">Features</MobileNavLink>
-            <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
-            <MobileNavLink href="#pricing">Pricing</MobileNavLink>
+            {/*<MobileNavLink href="#features">Features</MobileNavLink>*/}
+            {/*<MobileNavLink href="#testimonials">Testimonials</MobileNavLink>*/}
+            {/*<MobileNavLink href="#pricing">Pricing</MobileNavLink>*/}
+            <MobileNavLink href="/register">Call Me</MobileNavLink>
+            {/*<MobileNavLink href="#testimonials">Testimonials</MobileNavLink>*/}
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/login">Sign in</MobileNavLink>
+            <MobileNavLink href={discordInvite}>Contact</MobileNavLink>
+            {/*<MobileNavLink href="/login">Sign in</MobileNavLink>*/}
           </Popover.Panel>
         </Transition.Child>
       </Transition.Root>
@@ -108,7 +111,7 @@ export function Header() {
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
             <div className="hidden md:block">
-              <NavLink href={discordInvite}>Made with ♥︎ by{' '} team 101</NavLink>
+              <NavLink href={discordInvite}>Made with ♥︎ by team 101</NavLink>
             </div>
             <Button href="/register" color="blue">
               <span>
